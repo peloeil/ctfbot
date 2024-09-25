c91b8650e86b9ddfade8d786ac3a625199ff2943
Shun Ota <peloeil20@gmail.com>
2024-09-25 21:20:41 +0900

    ctfbot: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ctfbot
+
+CTF 用の discord bot
+
+`.env` は絶対に commit に含めないでください。`.gitignore` に記載してはいますが、自分でも気をつけてください。
---
+++ src/ctfbot/main.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+import discord
+from discord.ext import commands
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+def main():
+    bot.run(os.getenv('DISCORD_TOKEN'))
+
+if __name__ == "__main__":
+    main()
