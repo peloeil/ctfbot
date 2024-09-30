faf7a0dc9a8eecefc1bebcb152031a834c14d22a
Shun Ota <peloeil20@gmail.com>
2024-09-30 14:55:51 +0900

    refactor: format and documentation

--- src/ctfbot/cogs/misc.py
+++ src/ctfbot/cogs/misc.py
@@ -2,6 +2,7 @@ import random
 import re
 from discord.ext import commands
 
+
 class Misc(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -14,35 +15,39 @@ class Misc(commands.Cog):
     async def hello(self, ctx):
         await ctx.send(f"Hello, {ctx.author.name}!")
 
-    @commands.command(description="Rolls a dice in NdN format")
-    async def roll(self, ctx, dice: str):
+    @commands.command(description="Rolls a dice in NdM format")
+    async def roll(self, ctx, dice: str = commands.parameter(default="1d100", description="NdM format string")):
         """
-        Rolls a dice in NdN format.
-        N is a positive integer in (0, 100].
-        The first N is the number of dice to roll, the second N is the number of sides on each die.
+        Rolls a dice in NdM format.
+
+        N is the number of dice to roll.
+        M is the number of sides on each die.
+
+        constraints:
+            N, M are integers.
+            0 < N <= 100
+            0 < M <= 100
+
         Example: !roll 3d6
         """
         dice_pattern = re.compile(r"^(\d+)d(\d+)$")
-        match = dice_pattern.match(dice)
-        if not match:
-            await ctx.send("Format must be in NdN! Example: !roll 3d6")
+        if not dice_pattern.match(dice):
+            await ctx.send("Format must be in NdN!\nExample: !roll 3d6")
             return
 
         rolls, limit = map(int, dice.split("d"))
 
-        if rolls <= 0 or limit <= 0:
-            await ctx.send("Both values must be positive!")
+        if rolls <= 0 or 100 < rolls or limit <= 0 or 100 < limit:
+            await ctx.send("Constraint violation")
             return
-        if rolls > 100:
-            await ctx.send("You can roll a maximum of 100 dice at once.")
-            return
-        if limit > 100:
-            await ctx.send("The maximum number of sides on a die is 100.")
+        if rolls == 1:
+            await ctx.send(f"1d100: {random.randint(1, limit)}")
             return
         results = [random.randint(1, limit) for _ in range(rolls)]
         total = sum(results)
         result_str = ", ".join(map(str, results))
-        await ctx.send(f"Rolls: {result_str}\nTotal: {total}")
+        await ctx.send(f"{rolls}d{limit}: {result_str}\nTotal: {total}")
+
 
 async def setup(bot):
     await bot.add_cog(Misc(bot))
--- src/ctfbot/main.py
+++ src/ctfbot/main.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 
+extensions = {"!": ["ctfbot.cogs.misc"]}
+
+
 class DiscordBot(commands.Bot):
     def __init__(self, prefix):
         intents = discord.Intents.default()
@@ -10,16 +13,18 @@ class DiscordBot(commands.Bot):
         super().__init__(command_prefix=prefix, intents=intents)
 
     async def setup_hook(self):
-        await self.load_extension("ctfbot.cogs.misc")
-        #await self.load_extension("ctfbot.cogs.ctf")
+        for cogs in extensions[self.command_prefix]:
+            await self.load_extension(cogs)
 
     async def on_ready(self):
         print(f"{self.user} has connected to Discord!")
 
+
 def main():
     load_dotenv()
     bot = DiscordBot(prefix="!")
-    bot.run(os.getenv('DISCORD_TOKEN'))
+    bot.run(os.getenv("DISCORD_TOKEN"))
+
 
 if __name__ == "__main__":
     main()
