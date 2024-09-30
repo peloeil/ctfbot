c69fae5b2cbe1d61b2540ddb3c78bcef8b18b083
Shun Ota <peloeil20@gmail.com>
2024-10-01 02:20:15 +0900

    fix: cogs module name in load, unload, reload command

--- src/ctfbot/bot/cogs/manage_cogs.py
+++ src/ctfbot/bot/cogs/manage_cogs.py
@@ -16,7 +16,7 @@ class ManageCogs(commands.Cog):
     @app_commands.checks.has_permissions(moderate_members=True)
     async def load(self, interaction, name: str):
         try:
-            await self.bot.load_extension(f"bot.cogs.{name}")
+            await self.bot.load_extension(f"ctfbot.bot.cogs.{name}")
             await interaction.response.send_message(f"Loaded {name}")
         except Exception as e:
             await interaction.response.send_message(f"Error loading {name}: {e}")
@@ -25,7 +25,7 @@ class ManageCogs(commands.Cog):
     @app_commands.checks.has_permissions(moderate_members=True)
     async def unload(self, interaction, name: str):
         try:
-            await self.bot.unload_extension(f"bot.cogs.{name}")
+            await self.bot.unload_extension(f"ctfbot.bot.cogs.{name}")
             await interaction.response.send_message(f"Unloaded {name}")
         except Exception as e:
             await interaction.response.send_message(f"Error unloading {name}: {e}")
@@ -34,7 +34,7 @@ class ManageCogs(commands.Cog):
     @app_commands.checks.has_permissions(moderate_members=True)
     async def reload(self, interaction, name: str):
         try:
-            await self.bot.reload_extension(f"bot.cogs.{name}")
+            await self.bot.reload_extension(f"ctfbot.bot.cogs.{name}")
             await interaction.response.send_message(f"Reloaded {name}")
         except Exception as e:
             await interaction.response.send_message(f"Error reloading {name}: {e}")
