5e7b8bb60f7cc89d24a62b5a1044cbf0351f2c98
Shun Ota <peloeil20@gmail.com>
2025-05-20 02:05:30 +0900

    chore: apply ruff

--- src/bot/cogs/alpacahack.py
+++ src/bot/cogs/alpacahack.py
@@ -3,16 +3,15 @@ import discord
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
 from datetime import time, timezone, timedelta
-
-
 import requests
 from bs4 import BeautifulSoup
+
 
 def get_alpacahack_solves(user):
     response = requests.get(f"https://alpacahack.com/users/{user}")
     soup = BeautifulSoup(response.content, features="html.parser")
-    tbody = soup.find("tbody",class_="MuiTableBody-root")
-    ret = f"```\n"
+    tbody = soup.find("tbody", class_="MuiTableBody-root")
+    ret = "```\n"
     ret += f"{user}\n"
     ret += f"{'CHALLENGE':20}{'SOLVES':20}{'SOLVED AT':20}\n"
     for i in tbody.find_all("tr"):
