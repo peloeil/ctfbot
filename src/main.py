001fa5f96212ebd3b09e33d59f2777a2d76ac3f0
Shun Ota <peloeil20@gmail.com>
2026-06-17 22:58:34 +0900

    revert: remove unspecified bot entrypoint

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
-from bot.__main__ import main
+from bot.app import create_bot, run_bot
+
+
+def main() -> None:
+    bot = create_bot()
+    run_bot(bot)
+
 
 if __name__ == "__main__":
     main()
